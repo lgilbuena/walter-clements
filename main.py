a1ee22c16d668c8cdac80c699d77a4e2fcc02923
lgilbuena <jol0private@gmail.com>
2021-07-23 17:19:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import random
 client = discord.Client()
 responses = ['walter', 'i like fire trucks', 'i like moster trucks', 'i like fire trucks and moster trucks']
 trucks = ['https://gph.is/g/EGyJx5o','http://gph.is/1UTFMB3']
+walterBall = ['It is certain','It is decidedly so','Without a doubt','Yes, definitely','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes','Reply hazy try again','Ask again later',
+'Better not tell you now','Cannot predict now','Concentrate and ask again','Don''t count on it','My reply is no','My sources say no','Outlook not so good','Very doubtful','walter','i like fire trucks','i like moster trucks','i like fire trucks and moster trucks']
 
 @client.event
 async def on_ready():
@@ -15,6 +17,8 @@ async def on_message(message):
   resline = responses[random.randint(0,3)]
   if message.author == client.user:
     return 
+  if message.content.startswith('#walterBall'):
+    await message.channel.send(walterBall[random.randint(0,len(walterBall))])
   if message.content.startswith('#roll'):
     number = int(message.content.split('#roll',1)[1])
     await message.channel.send(random.randint(0,number))

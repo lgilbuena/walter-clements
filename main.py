0812408d3ec6a8bdba373179d98257400806f100
lgilbuena <jol0private@gmail.com>
2021-07-24 12:37:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import discord
+from discord.ext import commands
 import os
 import random
 
-client = discord.Client()
+client = commands.bot(command_prefix='#')
 responses = ['walter', 'i like fire trucks', 'i like moster trucks', 'i like fire trucks and moster trucks']
 trucks = ['https://gph.is/g/EGyJx5o','http://gph.is/1UTFMB3']
 walterBall = ['It is certain','It is decidedly so','Without a doubt','Yes, definitely','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes','Reply hazy try again','Ask again later',
 'Better not tell you now','Cannot predict now','Concentrate and ask again','Don''t count on it','My reply is no','My sources say no','Outlook not so good','Very doubtful','walter','i like fire trucks','i like moster trucks','i like fire trucks and moster trucks']
+@client.command(name='walterball')
+async def walterball(context,question):
+  await context.send(walterBall[random.randint(0,len(walterBall))])
+@client.command(name='roll')
+async def roll(context, number):
+  await context.send(random.randint(1,number))
+
 
 @client.event
 async def on_ready():
@@ -19,14 +27,6 @@ async def on_message(message):
     return 
   if random.randint(0, 100) == 69:
     await message.channel.send(resline)
-  elif message.content.startswith('#'):
-    tempstring = message.content.split('#')[1]
-    realstring = tempstring.split()[0].lower()
-    if "roll" in realstring:
-      number = int(message.content.split('#roll',1)[1])
-      await message.channel.send(random.randint(1,number))
-    elif "walterball" in realstring:
-      await message.channel.send(walterBall[random.randint(0,len(walterBall))])
   elif message.content.lower().startswith(responses[3]):
     await message.channel.send(resline)
   elif message.content.lower().startswith(responses[1]):
